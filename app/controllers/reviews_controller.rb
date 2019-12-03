f722d8ff15a07030fcfeb90712a63a4f429e76a7
jakarai <jakaraiforsythe@gmail.com>
2019-12-03 18:55:12 -0500

    review in progress

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,29 @@
 class ReviewsController < ApplicationController
+    def index
+        @user = User.find(session[:user])
+        @reviews = @user.reviews 
+    end
+
+    def show
+        @user = User.find(session[:user])
+        @review = Review.find(params[:id])
+    end
+
+    def new
+        @review = Review.new(review_params)
+    end
+    def create
+        review = Review.create(review_params)
+        if review
+        redirect_to reviews_path
+        else
+            redirect_to new_review_path
+        end
+    end
+    
+    private
+
+    def review_params
+       params.require(:reviews).permit(:content,:rating,:bottle_id,:user_id) 
+    end
 end
