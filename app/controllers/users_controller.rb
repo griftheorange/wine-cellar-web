12c55c3d0c11d63d0e407a56e5ab6cd38eb51593
Griffin Poole <griffinpoole5@gmail.com>
2019-12-03 16:50:11 -0500

    Adjusted login menu to have more descriptive errors. Moving to main menu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
             success_log(@user)
         else
             flash[:no_match] = "There is no username/password matching that input! Stop drinking so much!"
-            redirect_to home_login_path
+            redirect_to app_login_path
         end
     end
 
@@ -17,9 +17,15 @@ class UsersController < ApplicationController
         @user = User.find_by(username: p[:username])
         if !@user
             @user = User.create(p)
-            success_log(@user)
+            if @user.valid?
+                success_log(@user)
+            else
+                !@user.errors.messages[:username].empty? ? flash[:create]=@user.errors.messages[:username][0] : flash[:create]=@user.errors.messages[:password][0]
+                byebug
+                redirect_to new_user_path
+            end
         else
-            flash[:already_exists] = "That username already exists, get more creative."
+            flash[:create] = "That username already exists, get more creative."
             redirect_to new_user_path
         end
     end
@@ -29,7 +35,7 @@ class UsersController < ApplicationController
     end
 
     def success_log(user)
-        session[:user] = user.username
+        session[:user] = user.id
         redirect_to app_home_path
     end
 
