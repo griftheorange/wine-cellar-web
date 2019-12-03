da9665179920439f81eb88bebd7b6375f8167d12
jakarai <jakaraiforsythe@gmail.com>
2019-12-03 15:38:46 -0500

    seed in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+b1 = Bottle.create(brand: "Barefoot",type: "Rose",color: "Pink", year: 5)
+b2 = Bottle.create(brand: "Dark Horse", type: "Rose" ,color: "Pink" ,year: 3)
+b3 = Bottle.create(brand: "Kim Crawford", type: "Sauvignon Blanc" ,color: "White" ,year: 12)
+b4 = Bottle.create(brand: "Sineanne", type: "Pinot Gris" ,color: "White" ,year: 10 )
+b5 = Bottle.create(brand: , type: ,color: ,year: )
+b6 = Bottle.create(brand: , type: ,color: ,year: )
+b7 = Bottle.create(brand: , type: ,color: ,year: )
+b8 = Bottle.create(brand: , type: ,color: ,year: )
+b9 = Bottle.create(brand: , type: ,color: ,year: )
+b10 = Bottle.create(brand: , type: ,color: ,year: )
