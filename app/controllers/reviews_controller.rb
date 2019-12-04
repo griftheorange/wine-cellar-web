3ea3f8336736f89433f3291c116dee56dcd11769
jakarai <jakaraiforsythe@gmail.com>
2019-12-04 16:53:56 -0500

    reviews crud

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,18 +12,24 @@ class ReviewsController < ApplicationController
     end
     
     def new
+        @user = User.find(session[:user])
         @review = Review.new
     end
 
     def create
 
-        review = Review.create(review_params)
+        @review = Review.create(review_params)
+    
         redirect_to reviews_path
     end
     
+    def destroy
+        Review.find_by(review_params).destroy
+        redirect_to reviews
+    end
     private
 
     def review_params
-       params.require(:reviews).permit(:content,:rating,:bottle_id) 
+       params.require(:review).permit(:content,:rating,:bottle_id, :user_id) 
     end
 end
