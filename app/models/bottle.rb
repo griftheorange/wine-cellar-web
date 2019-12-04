ad4c770167e0b2c44dd2d64d95e09dfd4ef9292f
Griffin Poole <griffinpoole5@gmail.com>
2019-12-04 17:38:06 -0500

    Finished create bottle with authentification

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -2,6 +2,10 @@ class Bottle < ApplicationRecord
     has_many :reviews, dependent: :destroy
     has_many :stashes, dependent: :destroy
     has_many :cellars, through: :stashes
+
+    validates :brand, :wine_type, :color, :year, presence: true
+    validates :year, inclusion: {in: 300..(Time.now.strftime("%Y").to_i), message: "Can't be earlier than 300AD or later than the present."}
+    validates :brand, uniqueness: {scope: [:wine_type, :color, :year], message: "Can't duplicate"}
     
     def bottle_string
         "#{self.brand}, #{self.wine_type}, Year: #{self.year}"
