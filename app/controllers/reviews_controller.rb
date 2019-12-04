859b7497e180d14717671f54fb70789970c9350f
jakarai <jakaraiforsythe@gmail.com>
2019-12-04 17:33:34 -0500

    finished reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -24,12 +24,14 @@ class ReviewsController < ApplicationController
     end
     
     def destroy
-        Review.find_by(review_params).destroy
-        redirect_to reviews
+        Review.find_by(id: params[:id]).destroy
+        redirect_to reviews_path
     end
+
     private
 
     def review_params
-       params.require(:review).permit(:content,:rating,:bottle_id, :user_id) 
+       params.require(:review).permit(:content,:rating,:bottle_id,:user_id) 
+       
     end
 end
